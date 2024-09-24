46c2ba73fc64ffd8863954260ce374ea37695ea4
eunjin11 <whdmswls9207@gmail.com>
2024-09-24 15:31:50 +0900

    feat/#1 : 색상 변경

--- app/components/recharts/BarChartContainer.tsx
+++ app/components/recharts/BarChartContainer.tsx
@@ -29,10 +29,10 @@ const BarChartContainer = ({ data }: ChartPropsType) => {
         <YAxis />
         <Tooltip content={<BarChartTooltip />} />
         <Legend />
-        <Bar dataKey="pv" fill="#8884d8" radius={[10, 10, 0, 0]} />
+        <Bar dataKey="uv" fill="#FFD1DB" radius={[10, 10, 0, 0]} />
         <Bar
-          dataKey="uv"
-          fill="#82ca9d"
+          dataKey="pv"
+          fill="#E81051"
           radius={[10, 10, 0, 0]}
           maxBarSize={40}
         />
